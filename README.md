a6ee046f9589c6ec52a62f4190e5f2ffc014e361
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-21 16:39:00 +0200

    Added add/delete book funtionalities

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # top-project-library
 This is my take on the project "Library", from The Odin Project.
+
+Trash icon from: https://pictogrammers.com/library/mdi/icon/delete/
--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-/* All of your book objects are going to be stored in a simple array, so add a 
-function to the script (not the constructor) that can take user’s input and 
-store the new book objects into an array */
 let myLibrary = [
-    ["The Hobbit", "J.R.R. Tolkien", "295", false],
-    ["La Odisea", "Homero", 250, true],
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        pages: "295",
+        isRead: false
+    },
+    {
+        title: "La Odisea",
+        author: "Homero",
+        pages: 250,
+        isRead: true
+    },
 ];
 
-const btnNewBook = document.querySelector("#btnNewBook");
-btnNewBook.addEventListener('click', () => {
-    cleanDisplay();
-    showForm();
-});
-
+addEventListenerBtnNewBook();
 displayBooks(myLibrary);
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
+function addEventListenerBtnNewBook() {
+    const btnNewBook = document.querySelector("#btnNewBook");
+    btnNewBook.addEventListener('click', () => {
+        cleanDisplay();
+        showForm();
+    });
+}
 
 function showForm() {
     const display = document.getElementById("booksDisplay");
@@ -56,7 +64,7 @@ function showForm() {
     labelIsRead.textContent = "Have you read it?"
     const inputIsRead = document.createElement('input');
     inputIsRead.setAttribute("type", "checkbox");
-    inputIsRead.setAttribute('id', "isRead");
+    inputIsRead.setAttribute('id', "checkIsRead");
     liIsRead.appendChild(labelIsRead);
     liIsRead.appendChild(inputIsRead);
     ul.appendChild(liIsRead);
@@ -66,7 +74,28 @@ function showForm() {
     const btnAddBook = document.createElement("input");
     btnAddBook.setAttribute("value", "Add New Book");
     btnAddBook.setAttribute("type", "button");
+    btnAddBook.setAttribute("id", "btnAddNewBook");
     display.appendChild(btnAddBook);
+
+    const btnNewBook = document.getElementById("btnNewBook");
+    btnNewBook.style.visibility = "hidden";
+
+    const btnAddNewBook = document.querySelector("#btnAddNewBook");
+    btnAddNewBook.addEventListener('click', () => {
+        const title = document.getElementById("inputTitle").value;
+        const author = document.getElementById("inputAuthor").value;
+        const pages = document.getElementById("inputPages").value;
+        const isRead = document.getElementById("checkIsRead").checked;
+
+        const book = {
+            title: title,
+            author: author,
+            pages: pages,
+            isRead: isRead
+        }
+
+        addBookToLibrary(book);
+    });
 }
 
 function cleanDisplay() {
@@ -76,6 +105,83 @@ function cleanDisplay() {
     }
 }
 
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    cleanDisplay();
+    displayBooks(myLibrary);
+
+    const btnNewBook = document.getElementById("btnNewBook");
+    btnNewBook.style.visibility = "visible";
+
+    addEventListenerBtnNewBook();
+}
+
+function displayBooks(bookArray) {
+    
+    const display = document.getElementById("booksDisplay");
+
+    bookArray.forEach((book, index) => {
+
+        const card = document.createElement("div");
+        card.setAttribute('class', 'card');
+
+        const titleP = document.createElement('p');
+        titleP.textContent = `Title: ${book.title}`;
+        card.appendChild(titleP);
+
+        const authorP = document.createElement('p');
+        authorP.textContent = `Author: ${book.author}`;
+        card.appendChild(authorP);
+
+        const pagesP = document.createElement('p');
+        pagesP.textContent = `Pages: ${book.pages}`;
+        card.appendChild(pagesP);
+
+        const isReadCheck = document.createElement('input');
+        const lblIsRead = document.createElement("label");
+        isReadCheck.setAttribute("type", `checkbox`);
+        isReadCheck.setAttribute("id", `checkIsRead${index}`);
+        isReadCheck.setAttribute("data-bookIndex", index);
+        
+        lblIsRead.textContent = "Have you read it?";
+        isReadCheck.checked = book.isRead;
+        card.appendChild(lblIsRead);
+        card.appendChild(isReadCheck);
+
+        const btnDeleteBook = document.createElement("input");
+        btnDeleteBook.setAttribute("type", "image");
+        btnDeleteBook.setAttribute("id", `btnDeleteBook${index}`);
+        btnDeleteBook.setAttribute("src", "icons/delete.svg");
+        btnDeleteBook.setAttribute("class", "icon");
+        card.appendChild(btnDeleteBook);
+
+        display.appendChild(card);
+
+        addEventListenerCheckIsRead(book, index);
+        addEventListenerDeleteBook(index);
+    });
+}
+
+function addEventListenerDeleteBook(index) {
+    const btnDeleteBook = document.querySelector(`#btnDeleteBook${index}`);
+    btnDeleteBook.addEventListener("click", () => {
+        myLibrary.splice(index, 1);
+        cleanDisplay();
+        addEventListenerBtnNewBook();
+        displayBooks(myLibrary);
+    });
+}
+
+function addEventListenerCheckIsRead(book, index) {
+    const checkIsRead = document.querySelector(`#checkIsRead${index}`);
+    checkIsRead.addEventListener('click', () => {
+        if (myLibrary[index].isRead === true) {
+            myLibrary[index].isRead = false
+        } else {
+            myLibrary[index].isRead = true;
+        }
+    });
+}
 
 // Book Constructor
 function Book(title, author, pages, isRead) {
@@ -88,38 +194,3 @@ function Book(title, author, pages, isRead) {
         return `${title} by ${author}, ${pages} pages, ${isRead ? "read." : "not read yet."}`;
     }
 } 
-
-function addBookToLibrary() {
-
-}
-
-/* Write a function that loops through the array and displays each book on the 
-page. You can display them in some sort of table, or each on their own “card”. 
-It might help for now to manually add a few books to your array so you can see 
-the display. */
-function displayBooks(bookArray) {
-    bookArray.forEach(book => {
-        const display = document.getElementById("booksDisplay");
-
-        const card = document.createElement("div");
-        card.setAttribute('class', 'card');
-
-        const titleP = document.createElement('p');
-        titleP.textContent = `Title: ${book[0]}`;
-        card.appendChild(titleP);
-
-        const authorP = document.createElement('p');
-        authorP.textContent = `Author: ${book[1]}`;
-        card.appendChild(authorP);
-
-        const pagesP = document.createElement('p');
-        pagesP.textContent = `Pages: ${book[2]}`;
-        card.appendChild(pagesP);
-
-        const wasReadP = document.createElement('p');
-        wasReadP.textContent = book[3] ? "Read." : "Not read yet."
-        card.appendChild(wasReadP);
-
-        display.appendChild(card);
-    });
-}
